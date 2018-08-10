10e2f952c0de9bdfb7f5dc5bd0de819d9d13f400
sarahjablonski <shapeshiftwithme@gmail.com>
2018-08-10 18:47:25 +0000

    automatically backed up by learn

--- recipe_hash.rb
+++ recipe_hash.rb
@@ -24,8 +24,7 @@ recipe_ingredients(perfect_10_recipe)
 
 #2: Write code that returns the amount of chocolate chips from perfect_10 ingredients that is needed in the recipe.
 def amount_of_chocolate_chips(perfect_10_recipe)
-  puts perfect_10_recipe[:mini_dark_chocolate_ch
-  ips]
+  puts perfect_10_recipe[:mini_dark_chocolate_chips]
 end
 
 amount_of_chocolate_chips(perfect_10_recipe)
